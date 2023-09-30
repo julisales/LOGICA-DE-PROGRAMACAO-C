f242d40dcb6dbb7b710f8f970ba73214d370e591
Juliane Sales <136769997+julisales@users.noreply.github.com>
2023-09-30 16:11:39 -0300

    Update and rename ex04.c to operacoes.c

--- operacoes.c
+++ operacoes.c
@@ -10,7 +10,7 @@
         float p = 0;
         float so = 0;
 
-        printf ("-----------------------------MÉDIA, SOMA, PRODUTO, MAIOR E MENOR VALOR ENTRE 2 VALORES----------------------------- \n");
+        printf ("-----------------------------MÃ‰DIA, SOMA, PRODUTO, MAIOR E MENOR VALOR ENTRE 2 VALORES----------------------------- \n");
         printf ("Digite o primeiro valor: \n");
         scanf ("%f", &n1);
         
@@ -24,18 +24,18 @@
         system ("cls || clear");
 
         printf ("**************************************************************\n");
-        printf ("* A média entre %.1f e %.1f é: %.2f                            *\n", n1, n2, m);
-        printf ("* A soma entre %.1f e %.1f é: %.1f                             *\n", n1, n2, so);           
-        printf ("* A multiplicação entre %.1f e %.1f é: %.1f                    *\n", n1, n2, p);
+        printf ("* A mÃ©dia entre %.1f e %.1f Ã©: %.2f                            *\n", n1, n2, m);
+        printf ("* A soma entre %.1f e %.1f Ã©: %.1f                             *\n", n1, n2, so);           
+        printf ("* A multiplicaÃ§Äƒo entre %.1f e %.1f Ã©: %.1f                    *\n", n1, n2, p);
         
         
         if (n1 > n2)
         {
-            printf ("*  O maior valor é %.1f e o menor valor é %.1f.\n           *", n1, n2);
+            printf ("*  O maior valor Ã© %.1f e o menor valor Ã© %.1f.\n           *", n1, n2);
         }
         else
         {
-           printf ("*  O maior valor é %.1f e o menor valor é %.1f.\n            *", n2, n1);
+           printf ("*  O maior valor Ã© %.1f e o menor valor Ã© %.1f.\n            *", n2, n1);
         }
         
         return 0;
