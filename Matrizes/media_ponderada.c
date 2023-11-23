0cbf2dc2fdee0de7859b97257d12fad0113e5d84
Juliane Sales <136769997+julisales@users.noreply.github.com>
2023-11-23 09:42:25 -0300

    move

--- Matrizes/media_ponderada.c
+++ Matrizes/media_ponderada.c
@@ -14,17 +14,17 @@ int main()
 
     for (i = 0; i < 3; i++)
     {
-        printf("Informe o nome da %d° disciplina: \n", i + 1);
+        printf("Informe o nome da %dÂ° disciplina: \n", i + 1);
         gets(disciplinas[i]);
         for (j = 0; j < 3; j++)
         {
-            printf("Informe a %d° nota: \n", j + 1);
+            printf("Informe a %dÂ° nota: \n", j + 1);
             scanf("%f", &notas[i][j]);
         }
 
         for (k = 0; k < 3; k++)
         {
-            printf("Informe o peso da %dª nota: \n", k + 1);
+            printf("Informe o peso da %dÂª nota: \n", k + 1);
             scanf("%f", &pesos[k]);
 
             soma_pesos += pesos[k];
@@ -38,13 +38,13 @@ int main()
 
     for (i = 0; i < 3; i++)
     {
-        printf("Nome da %d° disciplina: %s \n", i + 1, disciplinas[i]);
+        printf("Nome da %dÂ° disciplina: %s \n", i + 1, disciplinas[i]);
         printf ("\n");
         for(j = 0; j < 3; j++){
-        printf("%d° Nota: %.1f \n", j + 1, notas[i][j]);
+        printf("%dÂ° Nota: %.1f \n", j + 1, notas[i][j]);
         printf("\n");
         }
-        printf("Média Ponderada das notas: %.1f \n", media[i]);
+        printf("MÃ©dia Ponderada das notas: %.1f \n", media[i]);
     }
     return 0;
 }
